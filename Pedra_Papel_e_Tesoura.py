def9f44ea3dd8a4d2f11b09700633b8c374a1f71
AFTERBOOM <32421959+afterboom@users.noreply.github.com>
2023-06-11 21:54:19 -0300

    Update Pedra_Papel_e_Tesoura.py
    Mudanças de otimização

--- Pedra_Papel_e_Tesoura.py
+++ Pedra_Papel_e_Tesoura.py
@@ -1,6 +1,28 @@
-import random
-import time 
-import os
+from time import sleep
+from random import choice
+from os import system
+from sys import exit
+
+def quer_continuar():
+    resposta = input("[!] Pressione ENTER para permanecer jogando ou digite SAIR para parar de jogar> ").lower()
+    if resposta == "sair":
+        print("[!] Foi um prazer jogar com você!")
+        exit()
+    else:
+        system("clear")
+
+def checar_vitoria(pontos_jogador, pontos_maquina):
+    vencedor = None
+    if pontos_jogador == 10:
+        vencedor = "JOGADOR"
+    if pontos_maquina == 10:
+        vencedor = "COMPUTADOR"
+    if vencedor != None:
+        print(f"[~] A pontuação final foi {ponto_jogador} PONTOS jogador e {ponto_maquina} PONTOS maquina")
+        print(f"[+] O {vencedor} é o GRANDE VENCEDOR!!!!!!")
+        exit()
+    else:
+        quer_continuar()
 
 def main():
     print("** Vamos jogar Pedra, Papel ou Tesoura :D **")
@@ -17,143 +39,52 @@ def main():
             if escolha not in opcoes:
                 print("[!] Você precisa colocar uma opção válida")
         print("[!] Vou fazer uma escolha *BIP* *BOP*....")
-        time.sleep(3)
-        escolha_maquina = random.choice(opcoes)
+        sleep(1.5)
+        escolha_maquina = choice(opcoes)
 
         # ----  ---- Condições de EMPATE ----  ---- 
         if escolha_maquina == escolha:
             print("[!] Escolhemos a mesma coisa")
             print("[~] Essa rodada foi empate")
-            parar = input("<Pressione ENTER para permanecer jogando ou digite SAIR para parar de jogar> ").lower()
-            ponto_maquina+=1
-            ponto_jogador+=1
-            if parar != "sair":
-                os.system("clear")   
-            elif parar == "sair":
-                print("[!] Foi um prazer jogar com você") 
-                break   
-            if ponto_jogador == 10:
-                print(f"A pontuação final foi {ponto_jogador} PONTOS jogador e {ponto_maquina} PONTOS maquina")
-                print("O JOGADOR é o GRANDE VENCEDOR !!!!!!")
-                break
-            if ponto_maquina == 10:
-                print(f"A pontuação final foi {ponto_jogador} PONTOS jogador e {ponto_maquina} PONTOS maquina")
-                print("A MÁQUINA é a GRANDE VENCEDORA !!!!!!") 
-                break           		              
-                
+            ponto_maquina += 1
+            ponto_jogador += 1
+            checar_vitoria(ponto_jogador, ponto_maquina)
+
         #  ----  ---- Condições de Vitória de Jogador ----  ---- 
         elif escolha == "tesoura" and escolha_maquina == "papel":
             print(f"[!] Eu escolhi {escolha_maquina} e você {escolha}")
             print("[~] Você ganhou essa rodada")
-            parar = input("<Pressione ENTER para permanecer jogando ou digite SAIR para parar de jogar> ").lower()
-            ponto_jogador +=1
-            if parar != "sair":
-                os.system("clear") 
-            elif parar == "sair":
-                print("[!] Foi um prazer jogar com você") 
-                break   
-            if ponto_jogador == 10:
-                print(f"A pontuação final foi {ponto_jogador} PONTOS jogador e {ponto_maquina} PONTOS maquina")
-                print("O JOGADOR é o GRANDE VENCEDOR !!!!!!")
-                break
-            if ponto_maquina == 10:
-                print(f"A pontuação final foi {ponto_jogador} PONTOS jogador e {ponto_maquina} PONTOS maquina")
-                print("A MÁQUINA é a GRANDE VENCEDORA !!!!!!") 
-                break           		              
+            ponto_jogador += 1
+            checar_vitoria()
         elif escolha == "papel" and escolha_maquina == "pedra":
             print(f"[!] Eu escolhi {escolha_maquina} e você {escolha}")
             print("[~] Você ganhou essa rodada")
-            parar = input("<Pressione ENTER para permanecer jogando ou digite SAIR para parar de jogar> ").lower()
-            ponto_jogador +=1
-            if parar != "sair":
-                os.system("clear")  
-            elif parar == "sair":
-                print("[!] Foi um prazer jogar com você") 
-                break  
-            if ponto_jogador == 10:
-                print(f"A pontuação final foi {ponto_jogador} PONTOS jogador e {ponto_maquina} PONTOS maquina")
-                print("O JOGADOR é o GRANDE VENCEDOR !!!!!!")
-                break
-            if ponto_maquina == 10:
-                print(f"A pontuação final foi {ponto_jogador} PONTOS jogador e {ponto_maquina} PONTOS maquina")
-                print("A MÁQUINA é a GRANDE VENCEDORA !!!!!!") 
-                break          		                     
+            ponto_jogador += 1
+            checar_vitoria(ponto_jogador, ponto_maquina)
         elif escolha == "pedra" and escolha_maquina == "tesoura":
             print(f"[!] Eu escolhi {escolha_maquina} e você {escolha}")
             print("[~] Você ganhou essa rodada")
-            parar = input("<Pressione ENTER para permanecer jogando ou digite SAIR para parar de jogar> ").lower()
-            ponto_jogador +=1
-            if parar != "sair":
-                os.system("clear") 
-            elif parar == "sair":
-                print("[!] Foi um prazer jogar com você") 
-                break    
-            if ponto_jogador == 10:
-                print(f"A pontuação final foi {ponto_jogador} PONTOS jogador e {ponto_maquina} PONTOS maquina")
-                print("O JOGADOR é o GRANDE VENCEDOR !!!!!!")
-                break
-            if ponto_maquina == 10:
-                print(f"A pontuação final foi {ponto_jogador} PONTOS jogador e {ponto_maquina} PONTOS maquina")
-                print("A MÁQUINA é a GRANDE VENCEDORA !!!!!!") 
-                break           		             
-                
+            ponto_jogador += 1
+            checar_vitoria(ponto_jogador, ponto_maquina)
+
         #  ----  ---- Condições de Derrota do Jogador ----  ---- 
         elif escolha_maquina == "tesoura" and escolha == "papel":
             print(f"[!] Eu escolhi {escolha_maquina} e você {escolha}")
             print("[~] Você perdeu essa rodada")
-            parar = input("<Pressione ENTER para permanecer jogando ou digite SAIR para parar de jogar> ").lower()
-            ponto_maquina +=1
-            if parar != "sair":
-                os.system("clear") 
-            elif parar == "sair":
-                print("[!] Foi um prazer jogar com você") 
-                break 
-            if ponto_jogador == 10:
-                print(f"A pontuação final foi {ponto_jogador} PONTOS jogador e {ponto_maquina} PONTOS maquina")
-                print("O JOGADOR é o GRANDE VENCEDOR !!!!!!")
-                break
-            if ponto_maquina == 10:
-                print(f"A pontuação final foi {ponto_jogador} PONTOS jogador e {ponto_maquina} PONTOS maquina")
-                print("A MÁQUINA é a GRANDE VENCEDORA !!!!!!") 
-                break     
+            ponto_maquina += 1
+            checar_vitoria(ponto_jogador, ponto_maquina)
         elif escolha_maquina == "papel" and escolha == "pedra":
             print(f"[!] Eu escolhi {escolha_maquina} e você {escolha}")
-            print("[~] Você perdeu essa rodada")
-            parar = input("<Pressione ENTER para permanecer jogando ou digite SAIR para parar de jogar> ").lower()
-            ponto_maquina +=1
-            if parar != "sair":
-                os.system("clear") 
-            elif parar == "sair":
-                print("[!] Foi um prazer jogar com você") 
-                break 
-            if ponto_jogador == 10:
-                print(f"A pontuação final foi {ponto_jogador} PONTOS jogador e {ponto_maquina} PONTOS maquina")
-                print("O JOGADOR é o GRANDE VENCEDOR !!!!!!")
-                break
-            if ponto_maquina == 10:
-                print(f"A pontuação final foi {ponto_jogador} PONTOS jogador e {ponto_maquina} PONTOS maquina")
-                print("A MÁQUINA é a GRANDE VENCEDORA !!!!!!") 
-                break       
+            print("[~] Você perdeu essa rodada!")
+            ponto_maquina += 1
+            checar_vitoria(ponto_jogador, ponto_maquina)
         elif escolha_maquina == "pedra" and escolha == "tesoura":
             print(f"[!] Eu escolhi {escolha_maquina} e você {escolha}")
             print("[~] Você perdeu essa rodada")
-            parar = input("<Pressione ENTER para permanecer jogando ou digite SAIR para parar de jogar> ").lower()
-            ponto_maquina +=1
-            if parar != "sair":
-                os.system("clear") 
-            elif parar == "sair":
-                print("[!] Foi um prazer jogar com você") 
-                break 
-            if ponto_jogador == 10:
-                print(f"A pontuação final foi {ponto_jogador} PONTOS jogador e {ponto_maquina} PONTOS maquina")
-                print("O JOGADOR é o GRANDE VENCEDOR !!!!!!")
-                break
-            if ponto_maquina == 10:
-                print(f"A pontuação final foi {ponto_jogador} PONTOS jogador e {ponto_maquina} PONTOS maquina")
-                print("A MÁQUINA é a GRANDE VENCEDORA !!!!!!") 
-                break                                                  
+            ponto_maquina += 1
+            checar_vitoria(ponto_jogador, ponto_maquina)
         else:
-            print("[!] Algo deu errado")
+            print("[!] Algo deu errado!")
             break
 
 if __name__ == "__main__":
